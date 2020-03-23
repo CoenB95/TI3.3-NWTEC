ad4683ae85acc2b72deee49e3206c880832c3187
CoenB95 <boelhouwers.coen@gmail.com>
2020-03-23 16:07:48 +0000

    Fix. Also spruce up the visuals.

--- network-setup.sh
+++ network-setup.sh
@@ -7,7 +7,8 @@
 for i in $(seq 1 10)
 do
   clear
-  echo "[0/7] Cleanup.."
+  echo -e "\e[94m[0/7] Cleanup..\e[0m"
+  echo -e "\e[33mCheck IP..\e[0m"
   ONLINE=$(ifconfig eth0 | grep -c 'inet')
   if [ $ONLINE == 1 ]
   then
@@ -23,69 +24,73 @@ ifconfig eth0 | grep -B1 -w inet | awk '{print $1, $2}'
 
 # Kill all 'udhcpd' process.
 DHCP_PROCESS=$(ps | grep -m 1 "[u]dhcpd" | awk '{print $1}')
-while [ ! -z "$DHCP_PROCESS" = "" ]
+echo -e "\e[33mKill running 'udhcp' processes..\e[0m"
+while [ ! -z "$DHCP_PROCESS" ]
 do
-  echo "Kill udhcp PID=$DHCP_PROCESS"
+  echo -e "\e[33m- Kill udhcp PID=$DHCP_PROCESS\e[0m"
   sudo kill $DHCP_PROCESS
-  sleep 1.0
+  sleep 0.2
   DHCP_PROCESS=$(ps | grep -m 1 "[u]dhcpd" | awk '{print $1}')
 done
-echo "Done."
-sleep 2.0
 
 # Kill all 'named' processes.
 NAMED_PROCESS=$(ps | grep -m 1 "[n]amed" | awk '{print $1}')
-while [ ! -z "$NAMED_PROCESS" = "" ]
+echo -e "\e[33mKill running 'named' processes..\e[0m"
+while [ ! -z "$NAMED_PROCESS" ]
 do
-  echo "Kill named PID=$NAMED_PROCESS"
+  echo -e "\e[33mKill named PID=$NAMED_PROCESS\e[0m"
   sudo kill $NAMED_PROCESS
   sleep 1.0
   NAMED_PROCESS=$(ps | grep -m 1 "[n]amed" | awk '{print $1}')
 done
-echo "Done."
+echo -e "\e[32mDone cleaning up.\e[0m"
 sleep 2.0
 
 # Run the ifconfig-setup.
 clear
-echo "[1/7] Setup Ethernet.."
+echo -e "\e[94m[1/7] Setup Ethernet..\e[0m"
 sudo ./dhcp/ethernet-setup.sh
+echo -e "\e[32mDone.\e[0m"
 sleep 2.0
 
 # Start DHCP for eth1 and eth2.
 clear
-echo "[2/7] Start DHCP (udhcp).."
+echo -e "\e[94m[2/7] Start DHCP (udhcp)..\e[0m"
 sudo udhcpd -f dhcp/dhcp.conf &
 sudo udhcpd -f dhcp/dhcp2.conf &
 sleep 2.0
 
 # Copy latest DNS-files to root-directory.
 clear
-echo "[3/7] Prepare DNS (copy to /root).."
-echo "Copying..."
+echo -e "\e[94m[3/7] Prepare DNS (copy to /root)..\e[0m"
+echo -e "\e[33mCopying files...\e[0m"
 sudo cp -r ./root-bind/ /root/bind/
-echo "Done."
-sleep 1.0
+echo -e "\e[32mCopying files done.\e[0m"
+sleep 2.0
 
 # Start DNS.
 clear
-echo "[4/7] Setup DNS (named).."
+echo -e "\e[94m[4/7] Start DNS (named)..\e[0m"
 sudo named -c /root/bind/named.conf -g &
 sleep 4.0
 
 # Setup Firewall.
 clear
-echo "[5/7] Setup NAT (iptables).."
+echo -e "\e[94m[5/7] Setup NAT (iptables)..\e[0m"
 sudo ./nat/tables-setup.sh
+echo -e "\e[32mDone.\e[0m"
 sleep 2.0
 
 # Fix our own DNS-server reference.
 clear
-echo "[6/7] Set default DNS-server to ourself.."
+echo -e "\e[94m[6/7] Set default DNS-server to ourself..\e[0m"
 echo "nameserver 10.0.0.1" | sudo tee /etc/resolv.conf
+echo -e "\e[32mDone.\e[0m"
 sleep 2.0
 
 clear
-echo "[7/7] Done. Status report:"
-echo "- IP: " && ifconfig eth0
-echo "- Check DHCP: " && ps | grep dhcp
-echo "- Check DNS: " && ps | grep named
+echo -e "\e[32m[7/7] Done. Status report:\e[0m"
+echo -e "\e[33m- IP: \e[0m" && ifconfig eth0
+echo -e "\e[33m- Check DHCP: \e[0m" && ps | grep dhcp
+echo -e "\e[33m- Check DNS: \e[0m" && ps | grep named
+echo -e "\e[33m- Check Firewall: \e[0m" && sudo iptables -L -v
