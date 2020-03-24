93b01d8d25e5932bce6a89a009ff72789914885b
CoenB95 <boelhouwers.coen@gmail.com>
2020-03-24 17:21:43 +0100

    [Untested] Create TFTP dir if missing.

--- dhcp/tftp-setup.sh
+++ dhcp/tftp-setup.sh
@@ -4,6 +4,18 @@ IP=10.0.0.1
 PORT=69
 ROOT_DIR=/remote-boot
 
+# Setup TFTP files
+if [ -e /remote-boot ]
+then
+  echo -e "\e[33m  TFTP directory missing; creating..\e[0m"
+  sleep 1.0
+  sudo mkdir /var/lib/misc
+  sudo touch /var/lib/misc/dhcp.leases
+else
+  echo -e "\e[32m  [OK] TFTP directory exists.\e[0m"
+  sleep 1.0
+fi
+
 # Start TFTP Server
 echo -e "\e[32m  [BG] TFTP started.\e[0m"
-udpsvd -vE $IP $PORT tftpd $ROOT_DIR
+sudo udpsvd -vE $IP $PORT tftpd $ROOT_DIR
