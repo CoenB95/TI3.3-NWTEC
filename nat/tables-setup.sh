9d80211d0a13b98db788c5bbbf55c4b595ea3171
CoenB95 <boelhouwers.coen@gmail.com>
2020-03-24 08:38:01 +0000

    Bugfix: Very wrong NAT.

--- nat/tables-setup.sh
+++ nat/tables-setup.sh
@@ -31,16 +31,8 @@
 
 echo "Setting up iptables..."
 echo 1 > /proc/sys/net/ipv4/ip_forward
-# iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-# iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables -A FORWARD -i eth0 -o eth2 -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
-# iptables -A FORWARD -i eth2 -o eth0 -j ACCEPT
 iptables -F
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth2 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth1 -o eth2 -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth0 -j ACCEPT
-iptables -A FORWARD -i eth2 -o eth1 -j ACCEPT
 echo "Setting up iptables done."
