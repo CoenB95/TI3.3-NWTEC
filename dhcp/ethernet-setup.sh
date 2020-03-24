f3aa7a031ae5de832fcb8161d11e5b31c174f7dc
CoenB95 <boelhouwers.coen@gmail.com>
2020-03-24 14:13:37 +0000

    [Works] Cleanup DHCP.

--- dhcp/ethernet-setup.sh
+++ dhcp/ethernet-setup.sh
@@ -14,14 +14,3 @@ fi
 # Setup eth1.
 ifconfig eth1 10.0.0.1 netmask 255.255.255.248 broadcast 10.0.0.15 up
 echo "Activated eth1"
-
-# Kill client eth2.
-if [ -f /var/run/udhcpc.eth2.pid ]
-then
-  kill `cat /var/run/udhcpc.eth2.pid`
-  echo "Killed eth2 process."
-fi
-
-# Setup eth2.
-ifconfig eth2 10.0.1.1 netmask 255.255.255.248 broadcast 10.0.0.15 up
-echo "Activated eth2"
