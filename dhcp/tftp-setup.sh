2813ffbe93505f67ed8d46b22bf097088a230bac
CoenB95 <boelhouwers.coen@gmail.com>
2020-03-25 20:32:03 +0000

    Implement TFTP server serving PiCore.

--- dhcp/tftp-setup.sh
+++ dhcp/tftp-setup.sh
@@ -4,17 +4,84 @@ IP=10.0.0.1
 PORT=69
 ROOT_DIR=/remote-boot
 
+# Kill all 'udpsvd' process.
+DHCP_PROCESS=$(ps | grep -m 1 "[u]dpsvd" | awk '{print $1}')
+echo -e "\e[33m  Kill running 'udhcpd' processes..\e[0m"
+while [ ! -z "$DHCP_PROCESS" ]
+do
+  echo -e "\e[34m  > Killed udpsvd process (PID=$DHCP_PROCESS).\e[0m"
+  sudo kill $DHCP_PROCESS
+  sleep 0.5
+  DHCP_PROCESS=$(ps | grep -m 1 "[u]dpsvd" | awk '{print $1}')
+done
+echo -e "\e[32m  [OK] Done killing 'udpsvd' processes.\e[0m"
+sleep 2.0
+
 # Setup TFTP files
 if [ ! -d "$ROOT_DIR" ]
 then
   echo -e "\e[33m  TFTP directory missing; creating..\e[0m"
   sleep 1.0
-  sudo mkdir $ROOT_DIR
+  sudo mkdir ${ROOT_DIR}
 else
   echo -e "\e[32m  [OK] TFTP directory exists.\e[0m"
   sleep 1.0
 fi
 
+echo -e "\e[33m  Removing old files..\e[0m"
+sudo rm ${ROOT_DIR}/bootcode.bin
+sudo rm -r ${ROOT_DIR}/boot
+echo -e "\e[32m  [OK] Cleaning done. TFTP directory should be empty:\e[0m"
+ls -l ${ROOT_DIR}
+sleep 2.0
+
+echo -e "\e[33m  Downloading PiCore zip..\e[0m"
+sudo mkdir ${ROOT_DIR}/piCore
+sudo wget -P ${ROOT_DIR}/piCore http://tinycorelinux.net/9.x/armv6/releases/RPi/piCore-9.0.3.zip
+echo -e "\e[32m  [OK] Downloading done.\e[0m"
+sleep 2.0
+
+echo -e "\e[33m  Unzipping..\e[0m"
+sudo unzip ${ROOT_DIR}/piCore/piCore-9.0.3.zip -d ${ROOT_DIR}/piCore
+echo -e "\e[32m  [OK] Done. Files:\e[0m"
+ls -l ${ROOT_DIR}/piCore
+sleep 2.0
+
+echo -e "\e[33m  Loop Device..\e[0m"
+LOOP_LOCATION=$(sudo losetup -f)
+echo -e "\e[34m  > Loopback at $LOOP_LOCATION\e[0m"
+sudo losetup -P -f ${ROOT_DIR}/piCore/piCore-9.0.3.img
+echo -e "\e[32m  [OK] Loop Device ready.\e[0m"
+sleep 2.0
+
+echo -e "\e[33m  Mount..\e[0m"
+sudo mkdir -p ${ROOT_DIR}/temp/boot
+sudo mkdir -p ${ROOT_DIR}/temp/root
+sudo mount ${LOOP_LOCATION}p1 ${ROOT_DIR}/temp/boot
+sudo mount ${LOOP_LOCATION}p2 ${ROOT_DIR}/temp/root
+echo -e "\e[32m  [OK] Done. Files in /boot:\e[0m"
+ls -l ${ROOT_DIR}/temp/boot
+sleep 2.0
+
+echo -e "\e[33m  Copying files..\e[0m"
+sudo cp -r ${ROOT_DIR}/temp/boot ${ROOT_DIR} #Note: creates /remote-boot/boot directory on its own.
+sudo cp ${ROOT_DIR}/boot/bootcode.bin ${ROOT_DIR}
+echo -e "\e[32m  [OK] Copying done.\e[0m"
+sleep 2.0
+
+echo -e "\e[33m  Unmount..\e[0m"
+sudo umount ${ROOT_DIR}/temp/boot
+sudo umount ${ROOT_DIR}/temp/root
+echo -e "\e[32m  [OK] Done.\e[0m"
+sleep 2.0
+
+echo -e "\e[33m  Delete temporary files..\e[0m"
+sudo rm -r ${ROOT_DIR}/piCore
+sudo rm -r ${ROOT_DIR}/temp
+echo -e "\e[32m  [OK] Done. Files in TFTP-directory:\e[0m"
+ls -l ${ROOT_DIR}
+sleep 2.0
+
 # Start TFTP Server
 echo -e "\e[32m  [BG] TFTP started.\e[0m"
-sudo udpsvd -vE $IP $PORT tftpd $ROOT_DIR
+sudo udpsvd -vE $IP $PORT tftpd $ROOT_DIR/boot &
--- network-setup.sh
+++ network-setup.sh
@@ -9,8 +9,8 @@ do
   clear
   echo -e "\e[94m[0/7] Preparing..\e[0m"
   echo -e "\e[33m  Checking IP..\e[0m"
-  ONLINE=$(ifconfig eth0 | grep -c 'inet')
-  if [ $ONLINE == 1 ]
+  ONLINE=$(ifconfig eth0 | grep -c "inet")
+  if [ "$ONLINE" = "1" ]
   then
     echo -e "\e[32m  [OK] Online!"
     break
@@ -24,37 +24,43 @@ ifconfig eth0 | grep -B1 -w inet | awk '{print $1, $2}'
 
 # Setup Ethernet.
 clear
-echo -e "\e[94m[1/6] Setup Ethernet..\e[0m"
+echo -e "\e[94m[1/7] Setup Ethernet..\e[0m"
 ./dhcp/ethernet-setup.sh
 sleep 2.0
 
 # Start DHCP. Create file to dump leases if it doesn't exist yet.
 clear
-echo -e "\e[94m[2/6] Start DHCP (udhcp)..\e[0m"
+echo -e "\e[94m[2/7] Start DHCP (udhcp)..\e[0m"
 ./dhcp/dhcp-setup.sh
 sleep 2.0
 
 clear
-echo -e "\e[94m[3/6] Start DNS (named)..\e[0m"
+echo -e "\e[94m[3/7] Start DNS (named)..\e[0m"
 ./root-bind/bind-setup.sh
 sleep 2.0
 
 # Setup Firewall.
 clear
-echo -e "\e[94m[4/6] Setup NAT (iptables)..\e[0m"
+echo -e "\e[94m[4/7] Setup NAT (iptables)..\e[0m"
 ./nat/tables-setup.sh
 sleep 2.0
 
 # Fix our own DNS-server reference.
 clear
-echo -e "\e[94m[5/6] Set default DNS-server to ourself..\e[0m"
+echo -e "\e[94m[5/7] Set default DNS-server to ourself..\e[0m"
 echo "nameserver 10.0.0.1" | sudo tee /etc/resolv.conf
 echo -e "\e[32m  [OK] Done.\e[0m"
 sleep 2.0
 
 clear
-echo -e "\e[32m[6/6] Done. Status report:\e[0m"
+echo -e "\e[94m[6/7] Start TFTP..\e[0m"
+./dhcp/tftp-setup.sh
+sleep 2.0
+
+clear
+echo -e "\e[32m[6/7] Done. Status report:\e[0m"
 echo -e "\e[33m- IP: \e[0m" && ifconfig eth0 | grep -B1 -w inet | awk '{print $1, $2}'
-echo -e "\e[33m- Check DHCP: \e[0m" && ps | grep dhcp
-echo -e "\e[33m- Check DNS: \e[0m" && ps | grep named
+echo -e "\e[33m- Check DHCP: \e[0m" && ps | grep "[u]dhcp"
+echo -e "\e[33m- Check DNS: \e[0m" && ps | grep "[n]amed"
+echo -e "\e[33m- Check TFTP: \e[0m" && ps | grep "[u]dpsvd"
 echo -e "\e[33m- Check Firewall: \e[0m" && sudo iptables -L -v
