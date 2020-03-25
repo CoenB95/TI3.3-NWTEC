6d66298803a07a04ca8e6e4336e9e971b3918221
tehfrank <frankveraart13@gmail.com>
2020-03-25 12:46:04 +0100

    Update tables-setup.sh

--- nat/tables-setup.sh
+++ nat/tables-setup.sh
@@ -16,4 +16,29 @@ sudo iptables -A POSTROUTING -t nat -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 
+# Allows all loopback (lo0) traffic and drop all traffic to 127/8 that
+# doesn't use lo0
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT
+
+# Accepts all established inbound connections
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allows all outbound traffic
+sudo iptables -A OUTPUT -j ACCEPT
+
+# Allows HTTP and HTTPS connections from anywhere
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# Allows SSH connections
+sudo iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
+
+# Allow ping
+sudo iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
+
+# Reject all other inbound - default deny unless explicitly allowed policy:
+sudo iptables -A INPUT -j DROP
+sudo iptables -A FORWARD -j DROP
+
 echo -e "\e[32m  [OK] Done.\e[0m"
