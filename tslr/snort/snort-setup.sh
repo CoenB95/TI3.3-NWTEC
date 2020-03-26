30eb294cdfcdda4a42863d09baec3b40a09f3bae
CoenB95 <boelhouwers.coen@gmail.com>
2020-03-26 16:49:40 +0000

    Unknown changes.

--- tslr/snort/snort-setup.sh
+++ tslr/snort/snort-setup.sh
@@ -20,6 +20,8 @@ sleep 2.0
 # Unpack rules.
 echo -e "\e[33mUnzip IDS rules..\e[0m"
 tar -xvzf ${SNORT_DIR}/community-rules.tar.gz -C ${SNORT_DIR} # Creates ${SNORT_DIR}/community-rules dir.
+sudo sed -i "s/include \$RULE\_PATH/#include \$RULE\_PATH/" ${SNORT_DIR}/community-rules/snort.conf
+sudo cp ${SNORT_DIR}/community-rules/snort.conf /etc/snort
 echo -e "\e[32m[OK] Done.\e[0m"
 sleep 2.0
 
@@ -32,5 +34,5 @@ sleep 2.0
 
 # If snort is occrectly installed and the rules have been downloaded this should make it work as an IDS
 echo -e "\e[33mBooting up Snort\e[0m"
-snort -d -h 192.168.1.0/24 -l ./log -c ${SNORT_DIR}/community-rules/snort.conf
+sudo snort -d -h 192.168.1.0/24 -l ./log -c /etc/snort/snort.conf
 echo "Snort has been booted up"
